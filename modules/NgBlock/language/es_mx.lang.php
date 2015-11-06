1978d44441860f2db06a2f87f29726d1c799e686
opencubed <a.celepija@studiosynthesis.biz>
2015-11-06 17:06:55 +0100

    NgBlock Improvement regarding Elsatic config

---
+++ modules/NgBlock/language/es_mx.lang.php
@@ -0,0 +1,21 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * 
+ * ***********************************************************
+*  Module       : NgBlock
+*  Language     : ES Spanish
+*  Version      : vt5.4.0
+*  Created Date : 2012-02-27
+*  Author       : Odin Consultores S de RL de CV
+*  License      : MPL 1.1
+ ************************************************************************************/
+$mod_strings = Array(
+'NgBlock'=>'Ng Block'
+);
+?>
