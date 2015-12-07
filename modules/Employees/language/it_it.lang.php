3cb1d74102a4989a168c330ca684762dbf90b7a0
loridacito <l.cito@studiosynthesis.biz>
2015-12-07 17:10:15 +0100

    add new module employees

---
+++ modules/Employees/language/it_it.lang.php
@@ -0,0 +1,57 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'Employees' => 'Dipendenti',
+	'Name' => 'Nome Dipendente',
+	'SINGLE_Employees' => 'Dipendente',
+	'Employee ID' => 'Dipendente ID',
+	'Last Name' => 'Cognome',
+	'Employee No' => 'Dipendente No',
+	'Title' => 'Titolo',
+	'Organization Name' => '',
+	'Department Name' => 'Reparto',
+	'OfficePhone' => '',
+	'Mobile' => '',
+	'Home Phone' => '',
+	'Other Phone' => '',
+	'Fax' => '',
+	'Birthdate' => '',
+	'Email' => '',
+	'Mailing Street' => '',
+	'Mailing PO Box' => '',
+	'Mailing City' => '',
+	'Mailing State' => '',
+	'Mailing Postal Code' => '',
+	'Mailing Country' => '',
+	'Other Street' => '',
+	'Other PO Box' => '',
+	'Other City' => '',
+	'Other State' => '',
+	'Other Postal Code' => '',
+	'Other Country' => '',
+	'First Department' => '',
+	'Change 2^ Department' => '',
+	'Change 3^ Department' => '',
+	'Change 4^ Department' => '',
+	'Date Change Department' => '',
+	'Date Change 2^ Department' => '',
+	'Date Change 3^ Department' => '',
+	'Date Change 4^ Department' => '',
+	'LBL_EMPLOYEE_INFORMATION' => 'Informazione Dipendente',
+	'LBL_DEPARTMENT_INFORMATION' => 'Department Information',
+	'LBL_ADDRESS_INFORMATION' => 'Address Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Informazione Descrizione',
+	'Assigned To' => '',
+	'Created Time' => '',
+	'Modified Time' => '',
+	'Description' => '',
+);
+?>
