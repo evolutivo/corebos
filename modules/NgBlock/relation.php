9deb705cf75cb41c89d19b93b74161469512d151
opencubed <a.celepija@studiosynthesis.biz>
2016-05-30 11:07:28 +0200

    home ngblock widgets

--- modules/NgBlock/relation.php
+++ modules/NgBlock/relation.php
@@ -8,5 +8,8 @@ $sql=$adb->query("SELECT tabid,tablabel FROM vtiger_tab where isentitytype=1");
                      $content[$i]['tablabel']=$adb->query_result($sql,$i,'tablabel');
                      $content[$i]['tabtrans']=  getTranslatedString($adb->query_result($sql,$i,'tablabel'),$adb->query_result($sql,$i,'module'));
                    }
+                    $content[$i]['tabid']=0;
+                    $content[$i]['tablabel']='Home';
+                    $content[$i]['tabtrans']=  'Home';
        echo json_encode($content);
 ?>
