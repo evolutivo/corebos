f88e91ede592894524c8d7599837753cb352284c
Lorida Cito <l.cito@studiosynthesis.biz>
2017-01-16 14:12:57 +0100

    solve relatedlist problems

--- Smarty/libs/plugins/function.dvwidget.php
+++ Smarty/libs/plugins/function.dvwidget.php
@@ -16,6 +16,6 @@
  * @return string|null
  */
 function smarty_function_dvwidget(array $params, Smarty_Internal_Template $template) {
-echo vtlib_process_widget($template->_tpl_vars['CUSTOM_LINK_DETAILVIEWWIDGET'], $template->_tpl_vars);   
+echo vtlib_process_widget($template->getTemplateVars('CUSTOM_LINK_DETAILVIEWWIDGET'), $template->getTemplateVars());   
 }
 
