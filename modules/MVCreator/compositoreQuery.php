9f85f0b8ddddd70dc70e02b99f80c319bc776327
Lorida Cito <l.cito@studiosynthesis.biz>
2018-05-28 12:00:05 +0200

    fix second join query

--- modules/MVCreator/compositoreQuery.php
+++ modules/MVCreator/compositoreQuery.php
@@ -210,21 +210,40 @@ function showJoinArray($selField1, $selField2, $nameView, $stringaFields, $selTa
             $strQuery .= '<b> FROM </b>' . strtolower($selTab1[$i]) .' as '.strtolower($selTab1[$i]).'_0 join vtiger_crmentity CRM_'.strtolower($selTab1[$i]).'_0 on CRM_'.strtolower($selTab1[$i]).'_0.crmid='.strtolower($selTab1[$i]).'_0.'.$firsttblid.' and CRM_'.strtolower($selTab1[$i]).'_0.deleted=0 <b>'.$qjoin.''.$qjoincf.' </b><b>INNER JOIN </b> '.$selTab2[$i].' <b> as </b> ' . strtolower($selTab2[$i]).'_'.$index. '<b> ON </b>' . strtolower($selTab1[$i]).'_0.'. $selfld1 . '<b> = </b>' . strtolower($selTab2[$i]).'_'.$index. '.'. $selfld2.' join vtiger_crmentity CRM_'.strtolower($selTab2[$i]).'_'.$index.' on CRM_'.strtolower($selTab2[$i]).'_'.$index.'.crmid='.strtolower($selTab2[$i]).'_'.$index.'.'.$secondtblid.' and CRM_'.strtolower($selTab2[$i]).'_'.$index.'.deleted=0 <b>'.$q2join.''.$q2joincf.'</b>';
          //  if(count($selTab1)==1)
            // $strQuery .= inerJoionwithCrmentity($Moduls,$stringaFields,$index,strtolower($selTab1[$i]));
-//             if (($selTab2[$i] == "vtiger_account" || $selTab1[$i] == "vtiger_account" ) && $acc == 0) {
-//                $strQuery .= '<b> INNER </b> join vtiger_accountbillads as vtiger_accountbillads_'.$index.' <b> ON </b> vtiger_account_'.$index.'.accountid=vtiger_accountbillads_'.$index.'.accountaddressid';
-//                $strQuery .= ' <b> INNER join </b> vtiger_accountshipads as vtiger_accountshipads_'.$index.' <b> ON </b>  vtiger_account_'.$index.'.accountid=vtiger_accountshipads_'.$index.'.accountaddressid';
-//                $acc =$acc+ 1;
-//            }
-//            if (($selTab2[$i] == "vtiger_contactdetails" || $selTab1[$i] == "vtiger_contactdetails" ) && $cont == 0) {
-//                $strQuery .= ' <b> INNER JOIN </b> vtiger_contactaddress as vtiger_contactaddress_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index.'.contactid=vtiger_contactaddress_'.$index.'.contactaddressid';
-//                $strQuery .= '<b>  INNER JOIN </b> vtiger_contactsubdetails as vtiger_contactsubdetails_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index.'.contactid=vtiger_contactsubdetails_'.$index.'.contactsubscriptionid';
-//                $cont =$cont+ 1;
-//            }
-//            if (($selTab2[$i] == "vtiger_leaddetails" || $selTab1[$i] == "vtiger_leaddetails" ) && $lead == 0) {
-//                $strQuery .= ' <b> INNER JOIN </b>vtiger_leadaddress as vtiger_leadaddress_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index.'.leadid=vtiger_leadaddress_'.$index.'.leadaddressid';
-//                $strQuery .= '<b>  INNER JOIN </b>vtiger_leadsubdetails as vtiger_leadsubdetails_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index.'.leadid=vtiger_leadsubdetails_'.$index.'.leadsubscriptionid';
-//                $lead =$lead+ 1;
-//            }
+
+         if (($selTab2[$i] == "vtiger_account" || $selTab1[$i] == "vtiger_account" ) && $acc == 0) {
+                if(strtolower($selTab1[$i])=='vtiger_account'){
+                $index3 = 0;
+                }
+                else{
+                $index3 = $index;
+                }
+                $strQuery .= '<b> INNER </b> join vtiger_accountbillads as vtiger_accountbillads_'.$index.' <b> ON </b> vtiger_account_'.$index3.'.accountid=vtiger_accountbillads_'.$index.'.accountaddressid';
+                $strQuery .= ' <b> INNER join </b> vtiger_accountshipads as vtiger_accountshipads_'.$index.' <b> ON </b>  vtiger_account_'.$index3.'.accountid=vtiger_accountshipads_'.$index.'.accountaddressid';
+                $acc =$acc+ 1;
+            }
+            if (($selTab2[$i] == "vtiger_contactdetails" || $selTab1[$i] == "vtiger_contactdetails" ) && $cont == 0) {
+                if(strtolower($selTab1[$i])=='vtiger_contactdetails'){
+                $index3 = 0;
+                }
+                else{
+                $index3 = $index;
+                }
+                $strQuery .= ' <b> INNER JOIN </b> vtiger_contactaddress as vtiger_contactaddress_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index3.'.contactid=vtiger_contactaddress_'.$index.'.contactaddressid';
+                $strQuery .= '<b>  INNER JOIN </b> vtiger_contactsubdetails as vtiger_contactsubdetails_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index3.'.contactid=vtiger_contactsubdetails_'.$index.'.contactsubscriptionid';
+                $cont =$cont+ 1;
+            }
+            if (($selTab2[$i] == "vtiger_leaddetails" || $selTab1[$i] == "vtiger_leaddetails" ) && $lead == 0) {
+                if(strtolower($selTab1[$i])=='vtiger_leaddetails'){
+                $index3 = 0;
+                }
+                else{
+                $index3 = $index;
+                }
+                $strQuery .= ' <b> INNER JOIN </b>vtiger_leadaddress as vtiger_leadaddress_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index3.'.leadid=vtiger_leadaddress_'.$index.'.leadaddressid';
+                $strQuery .= '<b>  INNER JOIN </b>vtiger_leadsubdetails as vtiger_leadsubdetails_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index3.'.leadid=vtiger_leadsubdetails_'.$index.'.leadsubscriptionid';
+                $lead =$lead+ 1;
+            }
             $index2=$index;
             $index++;
         } else {
@@ -262,23 +281,23 @@ function showJoinArray($selField1, $selField2, $nameView, $stringaFields, $selTa
             $selfld2=$selFieldload2[$i];
             else
             $selfld2=$selField2[$i];
-            $strQuery .= '<b> INNER JOIN </b>'.$selTab2[$i].' <b> as </b> ' . $selTab2[$i].'_'.$index . '<b> ON </b>' . strtolower($selTab1[$i]).'_'.($index-1).'.' . $selfld1 . '<b> = </b>' . strtolower($selTab2[$i]).'_'.$index. '.'. $selfld2.' join vtiger_crmentity as CRM_'.$selTab2[$i].'_'.$index.' on CRM_'.$selTab2[$i].'_'.$index.'.crmid='.$selTab2[$i].'_'.$index.'.'.$secondtblid.' and CRM_'.$selTab2[$i].'_'.$index.'.deleted=0 <b>'.$qjoin1.' '.$qjoin1cf.'</b>';
+            $strQuery .= '<b> INNER JOIN </b>'.$selTab2[$i].' <b> as </b> ' . $selTab2[$i].'_'.$index . '<b> ON </b>' . strtolower($selTab1[$i]).'_0'.'.' . $selfld1 . '<b> = </b>' . strtolower($selTab2[$i]).'_'.$index. '.'. $selfld2.' join vtiger_crmentity as CRM_'.$selTab2[$i].'_'.$index.' on CRM_'.$selTab2[$i].'_'.$index.'.crmid='.$selTab2[$i].'_'.$index.'.'.$secondtblid.' and CRM_'.$selTab2[$i].'_'.$index.'.deleted=0 <b>'.$qjoin1.' '.$qjoin1cf.'</b>';
             //$strQuery1 .= '<b> INNER JOIN </b>' . $selTab2[$i] . '<b> ON </b>' ;//. strtolower($selTab1[$i]) . '.' . $selField1[$i] . '<b> = </b>' . strtolower($selTab2[$i]) . '.' . $selField2[$i];
-//            if ($selTab2[$i] == "vtiger_account" && $acc == 0) {
-//                $strQuery .= '<b> INNER </b> join vtiger_accountbillads as vtiger_accountbillads_'.$index.' <b> ON </b> vtiger_account_'.$index.'.accountid=vtiger_accountbillads_'.$index.'.accountaddressid';
-//                $strQuery .= ' <b> INNER join </b> vtiger_accountshipads as vtiger_accountshipads_'.$index.' <b> ON </b>  vtiger_account_'.$index.'.accountid=vtiger_accountshipads_'.$index.'.accountaddressid';
-//                $acc =$acc+ 1;
-//            }
-//            if ($selTab2[$i] == "vtiger_contactdetails" && $cont == 0) {
-//                $strQuery .= ' <b> INNER JOIN </b> vtiger_contactaddress as vtiger_contactaddress_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index.'.contactid=vtiger_contactaddress_'.$index.'.contactaddressid';
-//                $strQuery .= '<b>  INNER JOIN </b> vtiger_contactsubdetails as vtiger_contactsubdetails_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index.'.contactid=vtiger_contactsubdetails_'.$index.'.contactsubscriptionid';
-//                $cont =$cont+ 1;
-//            }
-//            if ($selTab2[$i] == "vtiger_leaddetails" && $lead == 0) {
-//                $strQuery .= ' <b> INNER JOIN </b>vtiger_leadaddress as vtiger_leadaddress_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index.'.leadid=vtiger_leadaddress_'.$index.'.leadaddressid';
-//                $strQuery .= '<b>  INNER JOIN </b>vtiger_leadsubdetails as vtiger_leadsubdetails_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index.'.leadid=vtiger_leadsubdetails_'.$index.'.leadsubscriptionid';
-//                $lead =$lead+ 1;
-//            }
+            if ($selTab2[$i] == "vtiger_account" && $acc == 0) {
+                $strQuery .= '<b> INNER </b> join vtiger_accountbillads as vtiger_accountbillads_'.$index.' <b> ON </b> vtiger_account_'.$index.'.accountid=vtiger_accountbillads_'.$index.'.accountaddressid';
+                $strQuery .= ' <b> INNER join </b> vtiger_accountshipads as vtiger_accountshipads_'.$index.' <b> ON </b>  vtiger_account_'.$index.'.accountid=vtiger_accountshipads_'.$index.'.accountaddressid';
+                $acc =$acc+ 1;
+            }
+            if ($selTab2[$i] == "vtiger_contactdetails" && $cont == 0) {
+                $strQuery .= ' <b> INNER JOIN </b> vtiger_contactaddress as vtiger_contactaddress_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index.'.contactid=vtiger_contactaddress_'.$index.'.contactaddressid';
+                $strQuery .= '<b>  INNER JOIN </b> vtiger_contactsubdetails as vtiger_contactsubdetails_'.$index.' <b> ON </b>  vtiger_contactdetails_'.$index.'.contactid=vtiger_contactsubdetails_'.$index.'.contactsubscriptionid';
+                $cont =$cont+ 1;
+            }
+            if ($selTab2[$i] == "vtiger_leaddetails" && $lead == 0) {
+                $strQuery .= ' <b> INNER JOIN </b>vtiger_leadaddress as vtiger_leadaddress_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index.'.leadid=vtiger_leadaddress_'.$index.'.leadaddressid';
+                $strQuery .= '<b>  INNER JOIN </b>vtiger_leadsubdetails as vtiger_leadsubdetails_'.$index.' <b> ON </b>  vtiger_leaddetails_'.$index.'.leadid=vtiger_leadsubdetails_'.$index.'.leadsubscriptionid';
+                $lead =$lead+ 1;
+            }
            //  $strQuery .= inerJoionwithCrmentity($Moduls,$stringaFields,$index,strtolower($selTab1[$i]));
             $index++;
         }
